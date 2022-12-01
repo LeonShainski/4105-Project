e3eb6f664b97566716915c5c5512a4a781fd7ccb
Leon Shainski <leon.shainski@gmail.com>
2022-12-01 15:55:43 -0500

    Redoing unit tests as imageCheck method has been redone

--- test_main.py
+++ test_main.py
@@ -1,7 +1,9 @@
-import main   # The code to test
+import main   
 
+#Testing images which include corrpt images, should have one corrupt image of a tire being found
 def test_checkImages():
     assert main.checkImages('4105-Project\images') == ['tire-corrupt.png']
 
+#Testing without corrupt images, should recieve empty array
 def test_checkImages():
     assert main.checkImages('4105-Project\dataset') == []
